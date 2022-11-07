7cb79cbdb4a77465f4aabf0ad9e1cd8c5fe5f52f
Joaquin Alcon <joaquinear21@gmail.com>
2022-11-07 19:42:33 -0400

    GITIGNORE

--- src/test/java/pages/todoLy/MenuProjectSection.java
+++ src/test/java/pages/todoLy/MenuProjectSection.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 public class MenuProjectSection {
 
     public Button menuIconButton= new Button(By.xpath("//div[contains(@style,'block')]/img"));
-    public Button editButton= new Button(By.xpath("/ul[@id=\"projectContextMenu\"]//a[text()='Edit']"));
+    public Button editButton= new Button(By.xpath("//ul[@id=\"projectContextMenu\"]//a[text()='Edit']"));
     public Button deleteButton= new Button(By.id("ProjShareMenuDel"));
 
 }
